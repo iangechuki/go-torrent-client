0d76a2bf65a5e8a7479ec6b0057445debf7a8db3
Ian Gechuki <iangechuki@gmail.com>
2024-12-13 17:44:13 +0300

    added tracker communication for fetching peers

---
+++ tracker.go
@@ -0,0 +1,54 @@
+package torrentfile
+
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+	"time"
+
+	"github.com/iangechuki/go-torrent-client/peers"
+	"github.com/jackpal/bencode-go"
+)
+
+type bencodeTrackerResp struct {
+	Interval int    `bencode:"interval"`
+	Peers    string `bencode:"peers"`
+}
+
+func (t *TorrentFile) buildTrackerURL(peerId [20]byte, port uint16) (string, error) {
+	base, err := url.Parse(t.Announce)
+	if err != nil {
+		return "", err
+	}
+	params := url.Values{
+		"info_hash":  {string(t.InfoHash[:])},
+		"peer_id":    {string(peerId[:])},
+		"port":       {fmt.Sprintf("%d", port)},
+		"uploaded":   {"0"},
+		"downloaded": {"0"},
+		"compact":    {"1"},
+		"left":       {fmt.Sprintf("%d", t.Length)},
+	}
+	base.RawQuery = params.Encode()
+	return base.String(), nil
+}
+
+func (t *TorrentFile) requestPeers(peerID [20]byte, port uint16) ([]peers.Peer, error) {
+	url, err := t.buildTrackerURL(peerID, port)
+	if err != nil {
+		return nil, err
+	}
+	c := &http.Client{Timeout: 15 * time.Second}
+	resp, err := c.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	trackerResp := bencodeTrackerResp{}
+	err = bencode.Unmarshal(resp.Body, &trackerResp)
+	if err != nil {
+		return nil, err
+	}
+	return peers.Unmarshal([]byte(trackerResp.Peers))
+}
